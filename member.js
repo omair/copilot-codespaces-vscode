3578b8433c798fcd6aaf9d017346b28619598206
Omair Sajid <omair@omairsajid.com>
2024-05-14 04:30:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS'],
+        details: function() {
+            this.skills.forEach(function(skill) {
+                console.log(`${this.name} knows ${skill}`);
+            });
+        }
+    };
+
+    member.details();
+}
